4e4da5f679c20827c27bbc2e862e2e45e01b1b02
Maarten Visscher <mail@maartenvisscher.nl>
2016-05-04 01:14:56 +0200

    Progress in Minesweeper client code

--- client/Minesweeper.js
+++ client/Minesweeper.js
@@ -1,12 +1,57 @@
+/**
+ * Game model:
+ * game: {
+ *   grid: [
+ *     [' ', ' ', '0', '1', 'm', ...], // A row
+ *     [' ', ...], // Next row
+ *     ...
+ *   ]
+ *   mineCount: 10,
+ *   timestamp: 123456789, // Game start moment
+ *   end: { // Null object when game is still ongoing
+ *     mines: [],
+ *     timestamp: 123457890
+ *   }
+ * }
+ */
+
 import React from 'react';
 
-var Grid = React.createClass({
+/**
+ * Renders the complete game.
+ */
+var Minesweeper = React.createClass({
+  propTypes: {
+    grid: React.PropTypes.arrayOf(React.PropTypes.arrayOf(React.PropTypes.string)).isRequired,
+    mines: React.PropTypes.arrayOf(React.PropTypes.shape({
+      x: React.PropTypes.number,
+      y: React.PropTypes.number
+    }))
+  },
   render: function() {
-    return ();
+    return (
+      <div>
+        <div>
+          <div>{remaining}</div>
+          <div><img src={smiley} /></div>
+          <div>{time}</div>
+        </div>
+        <div>
+          // Todo render all cells
+        </div>
+       </div>
+    );
   }
 });
 
+/**
+ * Renders a single cell.
+ */
 var Cell = React.createClass({
+  propTypes: {
+    children: React.PropTypes.oneOf([' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', 'm']).isRequired,
+    mine: React.PropTypes.bool
+  },
   handleClick: function(event) {
   },
   render: function() {
@@ -18,28 +63,4 @@ var Cell = React.createClass({
   }
 });
 
-var Counter = React.createClass({
-  render: function() {
-    return ();
-  }
-});
-
-var Face = React.createClass({
-  render: function() {
-    return ();
-  }
-});
-
-var Timer = React.createClass({
-  render: function() {
-    return ();
-  }
-});
-
-var Game = React.createClass({
-  render: function() {
-    return (
-        
-    );
-  }
-});
+export default Minesweeper;
